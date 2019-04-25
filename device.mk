ec8de909597e23d67e57d9ac2ef347ce1820d5e1
Stricted <info@stricted.net>
2019-04-25 19:03:45 +0100

    a5xelte: set PRODUCT_AAPT_CONFIG
    Change-Id: Id12f387282c28915bbc7d2d0da64fe9abd36f20f

--- device.mk
+++ device.mk
@@ -54,6 +54,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AdvancedDisplay
 
+# Graphics
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := xlarge
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
+
 # HIDL Manifest
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
