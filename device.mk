c7715fe5b3d931e09bfa89fb39b7f3739068b8e8
Danny Wood <danwood76@gmail.com>
2019-12-23 12:00:00 +0000

    a5xelte: fingerprint: switch to using the new HIDL based hal
    Change-Id: I70c3f33e1979637cee343601a1a0fa0074cacb86

--- device.mk
+++ device.mk
@@ -43,10 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    fingerprint.exynos5 \
     libbauthtzcommon_shim \
-    android.hardware.biometrics.fingerprint@2.1-impl \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.samsung
 
 # Camera
 PRODUCT_PACKAGES += \
