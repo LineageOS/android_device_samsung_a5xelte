1dedfb1e8a4ba0a5b3fbab22109ef17c4169167e
Danny Wood <danwood76@gmail.com>
2021-01-15 15:43:36 +0000

    a7xelte: un-commonise the audio effects configuration
    Change-Id: If6d936399e613f5f5f65451abd622d67e727fd30

--- device.mk
+++ device.mk
@@ -20,7 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
+    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
 
 PRODUCT_PACKAGES += \
